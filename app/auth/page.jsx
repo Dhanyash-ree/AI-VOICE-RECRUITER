a1da6dfb1749057883a29d0f0fc1577e43b61893
Dhanyashree <dhanyashree4741@gmail.com>
2025-08-24 12:13:34 +0530

    modified landing page

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -11,6 +11,9 @@ function Login() {
     const signInWithGoogle=async()=>{
         const {error} = await supabase.auth.signInWithOAuth({
           provider:'google',
+           options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
+    },
          
         }) 
         if(error){
